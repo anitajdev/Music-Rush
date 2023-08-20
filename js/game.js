75f27b300b9ce86589e5a0ff95b405030774c8e3
Anita <jelicanita7@gmail.com>
2023-08-20 21:52:48 +0200

    Moving tiles completed

--- js/game.js
+++ js/game.js
@@ -1,111 +1,179 @@
 // Get the required elements from the HTML
-const tilesContainer = document.getElementById("tiles");
+
 const scoreElement = document.getElementById("score");
-const resultBox = document.querySelector(".result_box");
-const finalScoreText = document.querySelector(".final_score_text");
-const encouragementText = document.querySelector(".encouragement-text");
-const restartButton = document.querySelector(".restart");
-const audioElement = document.getElementById("audio");
-const startButton = document.querySelector(".start-bttn"); //NEW ELEMENT
-const gameHeader = document.querySelector(".game-header"); // NEW ELEMENT
+const startButton = document.querySelector(".start-button"); 
+const gameHeader = document.querySelector(".game-header"); 
 
-// Game variables
-let score = 0;
-let tiles = [];
-let gameInterval;
 
-// NEW CHANGE - START THE GAME
+// START THE GAME
 
 startButton.addEventListener("click", function(){
   gameHeader.classList.replace("d-flex", "d-none");
-  startButton.classList.replace("d-block", "d-none");
+  startButton.style.display = "none";
   scoreElement.style.display = "block";
   startGame();
 });
 
-// Create tiles
-for (let i = 1; i <= 4; i++) {
-  const tile = document.createElement("div");
-  tile.classList.add("tile");
-  tile.dataset.index = i;
-  tile.addEventListener("click", () => handleClick(i));
-  tilesContainer.appendChild(tile);
-  tiles.push(tile);
-}
 
-// Handle tile clicks
-function handleClick(index) {
-  if (index === tiles[0].dataset.index) {
-    tiles[0].classList.add("clicked");
-    score++;
-    updateScore();
-    tiles.shift();
-    spawnTile();
-  } else {
-    endGame();
+/* PLAYING GAME */
+
+
+//tracks
+
+const track1 = document.querySelector(".game-track1");
+const track2 = document.querySelector(".game-track2");
+const track3 = document.querySelector(".game-track3");
+const track4 = document.querySelector(".game-track4");
+const track5 = document.querySelector(".game-track5");
+
+//tiles
+
+const tile1 = document.querySelector(".tile1");
+const tile2 = document.querySelector(".tile2");
+const tile3 = document.querySelector(".tile3");
+const tile4 = document.querySelector(".tile4");
+const tile5 = document.querySelector(".tile5");
+
+
+
+// // Game variables
+
+let startPos1 = -40;
+let startPos2 = -60;
+let startPos3 = -50;
+let startPos4 = -70;
+let startPos5 = -55;
+
+let endPos = 90; 
+let score = 0;
+
+let moving1 = setInterval(()=> {
+  startPos1++;
+
+  tile1.style.top = `${startPos1}%`;
+
+  if(startPos1 < 0){
+    tile1.style.opacity = "0";
+  } else{
+    tile1.style.opacity = "1";
   }
-}
 
-// Update score element
-function updateScore() {
-  scoreElement.textContent = score;
-}
-
-// Spawn a new tile
-function spawnTile() {
-  const tile = document.createElement("div");
-  tile.classList.add("tile");
-  const randomIndex = Math.floor(Math.random() * 4) + 1;
-  tile.dataset.index = randomIndex;
-  tile.addEventListener("click", () => handleClick(randomIndex));
-  tilesContainer.appendChild(tile);
-  tiles.push(tile);
-}
-
-// Start the game
-function startGame() {
-  score = 0;
-  tiles = [];
-  tilesContainer.innerHTML = "";
-  spawnTile();
-  updateScore();
-  resultBox.classList.add("d-none");
-  audioElement.play();
-  gameInterval = setInterval(moveTiles, 10);
-}
-
-// Move the tiles
-function moveTiles() {
-  tiles.forEach((tile) => {
-    const currentTop = parseInt(getComputedStyle(tile).top);
-    const newTop = currentTop + 1;
-    tile.style.top = `${newTop}px`;
-
-    if (newTop >= window.innerHeight) {
-      endGame();
-    }
-  });
-}
-
-// End the game
-function endGame() {
-  clearInterval(gameInterval);
-  audioElement.pause();
-  resultBox.classList.remove("d-none");
-  finalScoreText.textContent = `You've scored ${score} points`;
-  if (score >= 50) {
-    encouragementText.textContent = "UNBELIEVABLE";
-  } else {
-    encouragementText.textContent = "Nice try!";
+  if(startPos1 == 1){
+    tile1.style.borderRadius = "0px";
   }
-}
 
-// Restart the game
-restartButton.addEventListener("click", () => {
-  startGame();
-});
+  if(startPos1 == endPos){
+    tile1.style.borderRadius = `0px 0px 0px 27px`;
+    // clearInterval(moving1);
+    startPos1 =-40;
+  }
 
-// // Start the game when the page loads
-// document.addEventListener("DOMContentLoaded", () => {
-//   startGame();
-// });
+},70);
+
+
+let moving2 = setInterval(()=> {
+  startPos2++;
+
+  tile2.style.top = `${startPos2}%`;
+
+  if(startPos2 < 0){
+    tile2.style.opacity = "0";
+  } else{
+    tile2.style.opacity = "1";
+  }
+
+  if(startPos2 == endPos){
+    // clearInterval(moving2);
+    startPos2 =-60;
+  }
+
+},70);
+
+
+let moving3 = setInterval(()=> {
+
+  startPos3++;
+  tile3.style.top = `${startPos3}%`;
+
+  if(startPos3 < 0){
+    tile3.style.opacity = "0";
+  } else{
+    tile3.style.opacity = "1";
+  }
+
+  if(startPos3 == endPos){
+    // clearInterval(moving3);
+    startPos3 =-50;
+
+  }
+
+},70);
+
+
+let moving4 = setInterval(()=> {
+
+  startPos4++;
+  tile4.style.top = `${startPos4}%`;
+
+  if(startPos4 < 0){
+    tile4.style.opacity = "0";
+  } else{
+    tile4.style.opacity = "1";
+  }
+
+  if(startPos4 == endPos){
+    // clearInterval(moving4);
+    startPos4 = -70;
+  }
+
+},70);
+
+let moving5 = setInterval(()=> {
+
+  startPos5++;
+
+  tile5.style.top = `${startPos5}%`;
+
+  if(startPos5 == 1){
+    tile5.style.borderRadius = "0px";
+  }
+
+  if(startPos5 < 0){
+    tile5.style.opacity = "0";
+  } else{
+    tile5.style.opacity = "1";
+  }
+  
+  if(startPos5 == endPos){
+    tile5.style.borderRadius = `0px 0px 27px 0px`;
+    // clearInterval(moving5);
+    startPos5 = -55;
+  }
+
+},70);
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
