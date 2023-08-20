6a5bf576f6deb7cda566ea54e91ab30e6de1034f
Tamara Zivkovic <tamara.zivkovic009@gmail.com>
2023-08-20 14:20:03 +0200

    game.js

--- js/game.js
+++ js/game.js
@@ -1,23 +1,100 @@
-//elements from HTML
+// Get the required elements from the HTML
 const tilesContainer = document.getElementById("tiles");
 const scoreElement = document.getElementById("score");
-const audio = document.getElementById("audio");
-const outAudio = document.getElementById("out");
-const resultAudio = document.getElementById("result");
-const restartButton = document.querySelector(".restart");
 const resultBox = document.querySelector(".result_box");
 const finalScoreText = document.querySelector(".final_score_text");
+const encouragementText = document.querySelector(".encouragement-text");
+const restartButton = document.querySelector(".restart");
+const audioElement = document.getElementById("audio");
 
-let currentAudioSource = "";
-
+// Game variables
 let score = 0;
-let isGameOver = false;
+let tiles = [];
+let gameInterval;
 
-//Function to create new tiles
-function createTile() {
+// Create tiles
+for (let i = 1; i <= 4; i++) {
   const tile = document.createElement("div");
-  tile.addEventListener("click", () => onTileClick(tile));
-  return tile;
+  tile.classList.add("tile");
+  tile.dataset.index = i;
+  tile.addEventListener("click", () => handleClick(i));
+  tilesContainer.appendChild(tile);
+  tiles.push(tile);
 }
 
+// Handle tile clicks
+function handleClick(index) {
+  if (index === tiles[0].dataset.index) {
+    tiles[0].classList.add("clicked");
+    score++;
+    updateScore();
+    tiles.shift();
+    spawnTile();
+  } else {
+    endGame();
+  }
+}
 
+// Update score element
+function updateScore() {
+  scoreElement.textContent = score;
+}
+
+// Spawn a new tile
+function spawnTile() {
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+  const randomIndex = Math.floor(Math.random() * 4) + 1;
+  tile.dataset.index = randomIndex;
+  tile.addEventListener("click", () => handleClick(randomIndex));
+  tilesContainer.appendChild(tile);
+  tiles.push(tile);
+}
+
+// Start the game
+function startGame() {
+  score = 0;
+  tiles = [];
+  tilesContainer.innerHTML = "";
+  spawnTile();
+  updateScore();
+  resultBox.classList.add("d-none");
+  audioElement.play();
+  gameInterval = setInterval(moveTiles, 10);
+}
+
+// Move the tiles
+function moveTiles() {
+  tiles.forEach((tile) => {
+    const currentTop = parseInt(getComputedStyle(tile).top);
+    const newTop = currentTop + 1;
+    tile.style.top = `${newTop}px`;
+
+    if (newTop >= window.innerHeight) {
+      endGame();
+    }
+  });
+}
+
+// End the game
+function endGame() {
+  clearInterval(gameInterval);
+  audioElement.pause();
+  resultBox.classList.remove("d-none");
+  finalScoreText.textContent = `You've scored ${score} points`;
+  if (score >= 50) {
+    encouragementText.textContent = "UNBELIEVABLE";
+  } else {
+    encouragementText.textContent = "Nice try!";
+  }
+}
+
+// Restart the game
+restartButton.addEventListener("click", () => {
+  startGame();
+});
+
+// Start the game when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  startGame();
+});
