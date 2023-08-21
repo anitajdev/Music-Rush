a0ac5ec5bfb60ab22d6bf9d27a3ec4fdb6c3558f
Tamara Zivkovic <tamara.zivkovic009@gmail.com>
2023-08-21 17:21:28 +0200

    score

--- ai.js
+++ ai.js
@@ -1,283 +1,139 @@
-// Get song information from query parameters
-const urlParams = new URLSearchParams(window.location.search);
-const artist = urlParams.get("artist");
-const title = urlParams.get("title");
-const audioSrc = urlParams.get("audio");
+const start = document.querySelector(".start");
+const game = document.querySelector("#game");
+const sco   = document.getElementById("score");
+const audio = document.getElementById("audio");
+const ply = document.getElementById("play");
+const out = document.getElementById("out");
+const results = document.getElementById("result");
+const result_box = document.querySelector(".result_box");
+const restart = result_box.querySelector(".restart");
+const text = result_box.querySelector(".score_text");
+let a;
+let tos = 2400;
+let bool1 = bool2 = bool3 = bool4 = true;
+var count=1;
+var score = 0;
+var step = 0;
+var mar=randomMargin(), mar2;
 
-console.log(artist, title, audioSrc);
+function viewResult(){
+  game.style.display = "none";
+  results.play();
+  result_box.classList.add("activeResult");
+  text.innerText = "You've scored " + score + " points";
+}
 
-// Update header with song information
-const songTitleHeader = document.querySelector(".game-header-info h2");
-const artistNameHeader = document.querySelector(".game-header-info p");
-songTitleHeader.textContent = title;
-artistNameHeader.textContent = artist;
+restart.onclick = ()=>{
+  start.style.display = "block";
+  result_box.classList.remove("activeResult");
+  sco.innerText = 0;
+  audio.currentTime = 0;
+}
 
-// Load and play audio
-const audio = new Audio(audioSrc);
-
-// Get the required elements from the HTML
-
-const scoreElement = document.getElementById("score");
-const startButton = document.querySelector(".start-button");
-const gameHeader = document.querySelector(".game-header");
-
-// START THE GAME
-
-startButton.addEventListener("click", function () {
-  gameHeader.classList.replace("d-flex", "d-none");
-  startButton.style.display = "none";
+function startAudio(){
   audio.play();
-  startGame();
+}
+audio.addEventListener("ended", () => {
+  audio.currentTime = 0;
+  audio.play();
 });
 
-/* PLAYING GAME */
-
-//tracks
-
-const track1 = document.querySelector(".game-track1");
-const track2 = document.querySelector(".game-track2");
-const track3 = document.querySelector(".game-track3");
-const track4 = document.querySelector(".game-track4");
-const track5 = document.querySelector(".game-track5");
-
-//tiles
-
-const tile1 = document.querySelector(".tile1");
-const tile2 = document.querySelector(".tile2");
-const tile3 = document.querySelector(".tile3");
-const tile4 = document.querySelector(".tile4");
-const tile5 = document.querySelector(".tile5");
-
-// // Game variables
-
-let startPos1 = -40;
-let startPos2 = -60;
-let startPos3 = -50;
-let startPos4 = -70;
-let startPos5 = -55;
-
-let endPos = 90;
-let score = 0;
-let wrongClicks = 0;
-
-function startGame() {
-  let gameInterval;
-
-  let moving1 = setInterval(() => {
-    startPos1++;
-
-    tile1.style.top = `${startPos1}%`;
-
-    if (startPos1 < 0) {
-      tile1.style.opacity = "0";
-    } else {
-      tile1.style.opacity = "1";
-    }
-
-    if (startPos1 == 1) {
-      tile1.style.borderRadius = "0px";
-    }
-
-    if (startPos1 == endPos) {
-      tile1.style.borderRadius = `0px 0px 0px 27px`;
-      // clearInterval(moving1);
-      startPos1 = -40;
-    }
-
-    tile1.addEventListener("click", () => {
-      if (startPos1 >= 0) {
-        // Correct tile clicked
-        score++;
-        scoreElement.textContent = score;
-      } else {
-        // Wrong tile clicked
-        wrongClicks++;
-        if (wrongClicks === 5) {
-          endGame();
-        }
-      }
-    });
-  }, 70);
-
-  let moving2 = setInterval(() => {
-    startPos2++;
-
-    tile2.style.top = `${startPos2}%`;
-
-    if (startPos2 < 0) {
-      tile2.style.opacity = "0";
-    } else {
-      tile2.style.opacity = "1";
-    }
-
-    if (startPos2 == endPos) {
-      // clearInterval(moving2);
-      startPos2 = -60;
-    }
-    tile2.addEventListener("click", () => {
-      if (startPos1 >= 0) {
-        // Correct tile clicked
-        score++;
-        scoreElement.textContent = score;
-      } else {
-        // Wrong tile clicked
-        wrongClicks++;
-        if (wrongClicks === 5) {
-          endGame();
-        }
-      }
-    });
-  }, 70);
-
-  let moving3 = setInterval(() => {
-    startPos3++;
-    tile3.style.top = `${startPos3}%`;
-
-    if (startPos3 < 0) {
-      tile3.style.opacity = "0";
-    } else {
-      tile3.style.opacity = "1";
-    }
-
-    if (startPos3 == endPos) {
-      // clearInterval(moving3);
-      startPos3 = -50;
-    }
-    tile3.addEventListener("click", () => {
-      if (startPos1 >= 0) {
-        // Correct tile clicked
-        score++;
-        scoreElement.textContent = score;
-      } else {
-        // Wrong tile clicked
-        wrongClicks++;
-        if (wrongClicks === 5) {
-          endGame();
-        }
-      }
-    });
-  }, 70);
-
-  let moving4 = setInterval(() => {
-    startPos4++;
-    tile4.style.top = `${startPos4}%`;
-
-    if (startPos4 < 0) {
-      tile4.style.opacity = "0";
-    } else {
-      tile4.style.opacity = "1";
-    }
-
-    if (startPos4 == endPos) {
-      // clearInterval(moving4);
-      startPos4 = -70;
-    }
-    tile4.addEventListener("click", () => {
-      if (startPos1 >= 0) {
-        // Correct tile clicked
-        score++;
-        scoreElement.textContent = score;
-      } else {
-        // Wrong tile clicked
-        wrongClicks++;
-        if (wrongClicks === 5) {
-          endGame();
-        }
-      }
-    });
-  }, 70);
-
-  let moving5 = setInterval(() => {
-    startPos5++;
-
-    tile5.style.top = `${startPos5}%`;
-
-    if (startPos5 == 1) {
-      tile5.style.borderRadius = "0px";
-    }
-
-    if (startPos5 < 0) {
-      tile5.style.opacity = "0";
-    } else {
-      tile5.style.opacity = "1";
-    }
-
-    if (startPos5 == endPos) {
-      tile5.style.borderRadius = `0px 0px 27px 0px`;
-      // clearInterval(moving5);
-      startPos5 = -55;
-    }
-    tile5.addEventListener("click", () => {
-      if (startPos1 >= 0) {
-        // Correct tile clicked
-        score++;
-        scoreElement.textContent = score;
-      } else {
-        // Wrong tile clicked
-        wrongClicks++;
-        if (wrongClicks === 5) {
-          endGame();
-        }
-      }
-    });
-  }, 70);
+function speed(e){
+  a = setInterval(appendDiv, e);
 }
-
-gameInterval = setInterval(() => {
-  // Check if all tiles reached the end position
-  if (
-    startPos1 === endPos &&
-    startPos2 === endPos &&
-    startPos3 === endPos &&
-    startPos4 === endPos &&
-    startPos5 === endPos
-  ) {
-    clearInterval(gameInterval); // Clear the interval when the game ends
-    endGame(); // Call the endGame function
-  }
-}, 70);
-
-// End the game
-function endGame() {
-  clearInterval(gameInterval);
+function reset(){
+  bool1 = bool2 = bool3 = bool4 = true;
+}
+function outs(){
   audio.pause();
-
-  // Display result box
-  const resultBox = document.querySelector(".result_box");
-  resultBox.classList.remove("d-none");
-
-  // Update final score and encouragement text
-  const finalScoreText = document.querySelector(".final_score_text");
-  const encouragementText = document.querySelector(".encouragement-text");
-  finalScoreText.textContent = `You've scored ${score} points`;
-  if (score >= 50) {
-    encouragementText.textContent = "UNBELIEVABLE";
-  } else {
-    encouragementText.textContent = "Nice try!";
-  }
-
-  // Add click event listener to replay button
-  const replayButton = document.querySelector(".restart");
-  replayButton.addEventListener("click", () => {
-    resultBox.classList.add("d-none");
-    startGame();
-  });
+  out.play();
+  setTimeout(viewResult, 1000);
 }
 
-// End the game
-// function endGame() {
-//   clearInterval(gameInterval);
-//   audio.pause();
-//   resultBox.classList.remove("d-none");
-//   finalScoreText.textContent = `You've scored ${score} points`;
-//   if (score >= 50) {
-//     encouragementText.textContent = "UNBELIEVABLE";
-//   } else {
-//     encouragementText.textContent = "Nice try!";
-//   }
-// }
+function appendDiv(){
+  var ob = document.createElement("div");
 
-// Restart the game
-// restartButton.addEventListener("click", () => {
-//   startGame();
-// });
+  do{mar2 = randomMargin()}
+  while(mar == mar2){mar = mar2}
+
+  ob.style.marginLeft = mar2+"%";
+  setTimeout(moveDown, 100, ob);
+  ob.onclick = () =>{
+    ob.style.background = "rgba(0,0,0,0.2)"
+    updateScore();
+  }
+  if(score >= 70 && score < 150) step = 1;
+  else if(score >= 150 && score < 400) step = 2;
+  else if(score >= 400 && score < 800) step = 3;
+  else if(score >= 800) step = 4;
+  document.getElementById("tiles").prepend(ob);
+}
+
+function randomMargin(){return 25*Math.floor(Math.random()*4)}
+
+function moveDown(e){
+  e.classList.add("move");
+  if(step == 1){
+    e.classList.add("speedX1");
+    if(bool1 == true){
+      clearInterval(a);
+      speed(300);
+      reset();
+      bool1 = false;
+      tos = 1600;
+    }
+  } 
+  else if(step == 2){
+    e.classList.add("speedX2");
+    if(bool2 == true){
+      clearInterval(a);
+      speed(250);
+      reset();
+      bool2 = false;
+      tos = 1600;
+    }
+  }
+  else if(step == 3){
+    e.classList.add("speedX3");
+    if(bool3 == true){
+      clearInterval(a);
+      speed(200);
+      reset();
+      bool3 = false;
+      tos = 1200;
+    }
+  } else if(step == 4){
+    e.classList.add("speedX4");
+    if(bool4 == true){
+      clearInterval(a);
+      speed(160);
+      reset();
+      bool4 = false;
+      tos = 1000;
+    }
+  }
+  setTimeout(removeDiv, tos, e)
+}
+
+function updateScore(){
+  score++;
+  sco.innerText = score;
+}
+
+function removeDiv(e){
+  var bg = e.style.background;
+  if(bg == ""){
+    clearInterval(a);
+    outs();
+  }
+  e.remove()
+}
+
+start.querySelector("button").onclick = ()=>{
+  ply.play();
+  game.style.display = "block";
+  start.style.display = "none";
+  score = 0;
+  speed(400);
+  setTimeout(startAudio, 1000);
+}
--- js/game.js
+++ js/game.js
@@ -26,6 +26,7 @@ const tiles = document.querySelectorAll("#tile");
 
 tiles.forEach((tile) => {
   tile.addEventListener("click", deleteTiles);
+  tile.addEventListener("click", calculateScore);
 });
 
 // Delete tiles
@@ -39,8 +40,7 @@ function deleteTiles(e) {
 startButton.addEventListener("click", function () {
   gameHeader.classList.replace("d-flex", "d-none");
   startButton.style.display = "none";
-
-  //fast forward audio
+  scoreElement.style.display = "block";
   audio.currentTime += 7;
   audio.play();
   startGame();
@@ -72,154 +72,117 @@ let startPos3 = -50;
 let startPos4 = -70;
 let startPos5 = -55;
 
-let endPos = 90;
+let endPos = 85;
 let score = 0;
 
 function startGame() {
- 
-    let moving1 = setInterval(() => {
-      startPos1++;
+  let moving1 = setInterval(() => {
+    startPos1++;
 
-      tile1.style.top = `${startPos1}%`;
+    tile1.style.top = `${startPos1}%`;
 
-      if (startPos1 < 0) {
-        tile1.style.opacity = "0";
-      } else {
-        tile1.style.opacity = "1";
-      }
+    if (startPos1 < 0) {
+      tile1.style.opacity = "0";
+    } else {
+      tile1.style.opacity = "1";
+    }
 
-      if (startPos1 == 1) {
-        tile1.style.borderRadius = "0px";
-      }
+    if (startPos1 == 1) {
+      tile1.style.borderRadius = "0px";
+    }
 
-      if (startPos1 == endPos) {
-        tile1.style.borderRadius = `0px 0px 0px 27px`;
-        // clearInterval(moving1);
-        startPos1 = -40;
-        tile1.style.display = "block";
-      }
-    }, 30);
+    if (startPos1 == endPos) {
+      tile1.style.borderRadius = `0px 0px 0px 27px`;
+      // clearInterval(moving1);
+      startPos1 = -40;
+      tile1.style.display = "block";
+    }
+  }, 30);
 
-    let moving2 = setInterval(() => {
-      startPos2++;
+  let moving2 = setInterval(() => {
+    startPos2++;
 
-      tile2.style.top = `${startPos2}%`;
+    tile2.style.top = `${startPos2}%`;
 
-      if (startPos2 < 0) {
-        tile2.style.opacity = "0";
-      } else {
-        tile2.style.opacity = "1";
-      }
+    if (startPos2 < 0) {
+      tile2.style.opacity = "0";
+    } else {
+      tile2.style.opacity = "1";
+    }
 
-      if (startPos2 == endPos) {
-        // clearInterval(moving2);
-        startPos2 = -60;
-        tile2.style.display = "block";
-      }
-    }, 30);
+    if (startPos2 == endPos) {
+      // clearInterval(moving2);
+      startPos2 = -60;
+      tile2.style.display = "block";
+    }
+  }, 30);
 
-    let moving3 = setInterval(() => {
-      startPos3++;
-      tile3.style.top = `${startPos3}%`;
+  let moving3 = setInterval(() => {
+    startPos3++;
+    tile3.style.top = `${startPos3}%`;
 
-      if (startPos3 < 0) {
-        tile3.style.opacity = "0";
-      } else {
-        tile3.style.opacity = "1";
-      }
+    if (startPos3 < 0) {
+      tile3.style.opacity = "0";
+    } else {
+      tile3.style.opacity = "1";
+    }
 
-      if (startPos3 == endPos) {
-        // clearInterval(moving3);
-        startPos3 = -50;
-        tile3.style.display = "block";
-      }
-    }, 30);
+    if (startPos3 == endPos) {
+      // clearInterval(moving3);
+      startPos3 = -50;
+      tile3.style.display = "block";
+    }
+  }, 30);
 
-    let moving4 = setInterval(() => {
-      startPos4++;
-      tile4.style.top = `${startPos4}%`;
+  let moving4 = setInterval(() => {
+    startPos4++;
+    tile4.style.top = `${startPos4}%`;
 
-      if (startPos4 < 0) {
-        tile4.style.opacity = "0";
-      } else {
-        tile4.style.opacity = "1";
-      }
+    if (startPos4 < 0) {
+      tile4.style.opacity = "0";
+    } else {
+      tile4.style.opacity = "1";
+    }
 
-      if (startPos4 == endPos) {
-        // clearInterval(moving4);
-        startPos4 = -70;
-        tile4.style.display = "block";
-      }
-    }, 30);
+    if (startPos4 == endPos) {
+      // clearInterval(moving4);
+      startPos4 = -70;
+      tile4.style.display = "block";
+    }
+  }, 30);
 
-    let moving5 = setInterval(() => {
-      startPos5++;
+  let moving5 = setInterval(() => {
+    startPos5++;
 
-      tile5.style.top = `${startPos5}%`;
+    tile5.style.top = `${startPos5}%`;
 
-      if (startPos5 == 1) {
-        tile5.style.borderRadius = "0px";
-      }
+    if (startPos5 == 1) {
+      tile5.style.borderRadius = "0px";
+    }
 
-      if (startPos5 < 0) {
-        tile5.style.opacity = "0";
-      } else {
-        tile5.style.opacity = "1";
-      }
+    if (startPos5 < 0) {
+      tile5.style.opacity = "0";
+    } else {
+      tile5.style.opacity = "1";
+    }
 
-      if (tile5.classList.contains("clicked")) {
-        startPos5 = -55;
-      }
+    if (tile5.classList.contains("clicked")) {
+      startPos5 = -55;
+    }
 
-      if (startPos5 == endPos) {
-        tile5.style.borderRadius = `0px 0px 27px 0px`;
-        // clearInterval(moving5);
-        startPos5 = -55;
-        tile5.style.display = "block";
-      }
-    }, 30);
- 
+    if (startPos5 == endPos) {
+      tile5.style.borderRadius = `0px 0px 27px 0px`;
+      // clearInterval(moving5);
+      startPos5 = -55;
+      tile5.style.display = "block";
+    }
+  }, 30);
 }
 
-//RESULTS
+// Calculating score on the screen
 
-const resultBox = document.querySelector(".result_box");
-const finalScoreText = document.querySelector(".final_score_text span");
-const encouragementText = document.querySelector(".encouragement-text");
-const restartButton = document.querySelector(".restart");
-
-// Initialize game interval variable
-let gameInterval;
-
-// Add event listener to restart button
-restartButton.addEventListener("click", () => {
-  resultBox.classList.add("d-none");
-  resetGame();
-  startButton.style.display = "block";
-});
-
-// Reset the game state
-function resetGame() {
-  tiles.forEach((tile) => {
-    tile.style.display = "block";
-  });
-  startPos1 = -40;
-  startPos2 = -60;
-  startPos3 = -50;
-  startPos4 = -70;
-  startPos5 = -55;
-  score = 0;
-  scoreElement.textContent = score;
-}
-
-function endGame() {
-  clearInterval(gameInterval);
-  audio.pause();
-  resultBox.classList.remove("d-none");
-  finalScoreText.textContent = score;
-  if (score >= 5) {
-    encouragementText.textContent = "UNBELIEVABLE";
-  } else {
-    encouragementText.textContent = "Nice try!";
-  }
+let sco = 0;
+function calculateScore() {
+  sco++;
+  scoreElement.textContent = `${sco}`;
 }
